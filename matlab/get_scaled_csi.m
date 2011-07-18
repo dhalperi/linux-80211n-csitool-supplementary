da052cae09f0b51fa79bd8a42e63f9e591d8929d
Daniel Halperin <dhalperi@cs.washington.edu>
2011-07-18 15:26:38 -0700

    matlab: refactor get_total_rss
    Useful outside of get_scaled_csi, so separate it out and remove code
    redundancy.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- matlab/get_scaled_csi.m
+++ matlab/get_scaled_csi.m
@@ -9,17 +9,7 @@ function ret = get_scaled_csi(csi_st)
     % Calculate the scale factor between RSSI and CSI
     csi_sq = csi .* conj(csi);
     csi_mag = sum(csi_sq(:));
-    % Careful here: rssis could be zero
-    rssi_mag = 0;
-    if csi_st.rssi_a ~= 0
-        rssi_mag = rssi_mag + dbinv(csi_st.rssi_a);
-    end
-    if csi_st.rssi_b ~= 0
-        rssi_mag = rssi_mag + dbinv(csi_st.rssi_b);
-    end
-    if csi_st.rssi_c ~= 0
-        rssi_mag = rssi_mag + dbinv(csi_st.rssi_c);
-    end
+    rssi_mag = dbinv(get_total_rss(csi_st));
 
     % Noise might be undefined
     % ... If so, set it to -92
@@ -32,8 +22,8 @@ function ret = get_scaled_csi(csi_st)
     % Scale factor to convert to SNR. Two steps:
     %
     %   Scale CSI -> S : rssi_mag / (mean of csi_mag)
-    %   Calculate N: noise + 44 + AGC
-    scale = rssi_mag / (csi_mag / 30) / dbinv(noise + 44 + csi_st.agc);
+    %   N: noise in dBm
+    scale = rssi_mag / (csi_mag / 30) * dbinv(-noise);
     ret = csi * sqrt(scale);
     if csi_st.Ntx == 2
         ret = ret * sqrt(2);
--- matlab/get_scaled_csi_sm.m
+++ matlab/get_scaled_csi_sm.m
@@ -1,4 +1,6 @@
 %GET_SCALED_CSI_SM Converts a CSI struct to a scaled CSI matrix.
+% This version undoes Intel's spatial mapping to return the pure
+% MIMO channel matrix H.
 %
 % (c) 2008-2011 Daniel Halperin <dhalperi@cs.washington.edu>
 %
@@ -9,17 +11,7 @@ function ret = get_scaled_csi_sm(csi_st)
     % Calculate the scale factor between RSSI and CSI
     csi_sq = csi .* conj(csi);
     csi_mag = sum(csi_sq(:));
-    % Careful here: rssis could be zero
-    rssi_mag = 0;
-    if csi_st.rssi_a ~= 0
-        rssi_mag = rssi_mag + dbinv(csi_st.rssi_a);
-    end
-    if csi_st.rssi_b ~= 0
-        rssi_mag = rssi_mag + dbinv(csi_st.rssi_b);
-    end
-    if csi_st.rssi_c ~= 0
-        rssi_mag = rssi_mag + dbinv(csi_st.rssi_c);
-    end
+    rssi_mag = dbinv(get_total_rss(csi_st));
 
     % Noise might be undefined
     % ... If so, set it to -92
@@ -32,11 +24,11 @@ function ret = get_scaled_csi_sm(csi_st)
     % Scale factor to convert to SNR. Two steps:
     %
     %   Scale CSI -> S : rssi_mag / (mean of csi_mag)
-    %   Calculate N: noise + 44 + AGC
-    scale = rssi_mag / (csi_mag / 30) / dbinv(noise + 44 + csi_st.agc);
+    %   N: noise in dBm
+    scale = rssi_mag / (csi_mag / 30) * dbinv(-noise);
     ret = csi * sqrt(scale);
     if csi_st.Ntx == 2
-        ret = ret * sqrt(csi_st.Ntx);
+        ret = ret * sqrt(2);
     elseif csi_st.Ntx == 3
         ret = ret * sqrt(dbinv(4.5));
     end
