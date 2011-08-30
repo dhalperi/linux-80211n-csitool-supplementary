02af39eacc835c0eb85235fc82d89236dc978beb
Daniel Halperin <dhalperi@cs.washington.edu>
2011-08-30 16:58:19 -0700

    matlab: add quantization error to get_scaled_csi
    For strong links, CSI quantization error can dominate noise. Thus, it's
    important to take this into account when computing the noise floor of
    the H matrix.
    
    The quantization error is roughly 36 dB below RSS, but calculated in a
    way that reflects the scaling of a particular CSI matrix.
    
    This makes CSI much more accurate for strong links, improves algorithmic
    performance, and appears to get rid of the nasty 'Inf' Effective SNR
    values that complicate code.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- matlab/get_scaled_csi_sm.m
+++ matlab/get_scaled_csi_sm.m
@@ -1,38 +1,13 @@
-%GET_SCALED_CSI_SM Converts a CSI struct to a scaled CSI matrix.
+%GET_SCALED_CSI_SM Converts a CSI struct to a channel matrix H.
 % This version undoes Intel's spatial mapping to return the pure
 % MIMO channel matrix H.
 %
 % (c) 2008-2011 Daniel Halperin <dhalperi@cs.washington.edu>
 %
 function ret = get_scaled_csi_sm(csi_st)
-    % Pull out CSI
-    csi = csi_st.csi;
-
-    % Calculate the scale factor between RSSI and CSI
-    csi_sq = csi .* conj(csi);
-    csi_mag = sum(csi_sq(:));
-    rssi_mag = dbinv(get_total_rss(csi_st));
-
-    % Noise might be undefined
-    % ... If so, set it to -92
-    if (csi_st.noise == -127)
-        noise = -92;
-    else
-        noise = csi_st.noise;
-    end
-
-    % Scale factor to convert to SNR. Two steps:
-    %
-    %   Scale CSI -> S : rssi_mag / (mean of csi_mag)
-    %   N: noise in dBm
-    scale = rssi_mag / (csi_mag / 30) * dbinv(-noise);
-    ret = csi * sqrt(scale);
-    if csi_st.Ntx == 2
-        ret = ret * sqrt(2);
-    elseif csi_st.Ntx == 3
-        ret = ret * sqrt(dbinv(4.5));
-    end
+    % Normal procedure to scale normalized CSI to H
+    ret = get_scaled_csi(csi_st);
     
-    % Remove the spatial mapping that was used for this CSI
+    % Remove the spatial mapping that was used for this H matrix
     ret = remove_sm(ret, csi_st.rate);
 end
