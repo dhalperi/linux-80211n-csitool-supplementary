12cbebd538e8905d495464cf7dcbdadca05ae6c1
Daniel Halperin <dhalperi@cs.washington.edu>
2011-09-21 22:26:22 -0700

    injection: add scripts to setup, and README to explain
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

---
+++ injection/setup_inject.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sudo /bin/bash
+rmmod iwlagn mac80211 cfg80211
+modprobe iwlagn
+ifconfig wlan0 2>/dev/null 1>/dev/null
+while [ $? -ne 0 ]
+do
+	        ifconfig wlan0 2>/dev/null 1>/dev/null
+done
+iw dev wlan0 interface add mon0 type monitor
+iw mon0 set channel $1 $2
+ifconfig mon0 up
