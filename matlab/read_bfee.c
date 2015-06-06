45e0612fdd95b26486fc3d17443caee3a081d877
David Ward <david.ward@ll.mit.edu>
2015-06-06 16:56:33 -0400

    matlab: make read_bfee MEX-file C89-compliant for building on Windows
    The MSVC compiler returns an error when compiling C code (not C++)
    if variables are not all declared at the beginning of a function.
    
    Signed-off-by: David Ward <david.ward@ll.mit.edu>

--- matlab/read_bfee.c
+++ matlab/read_bfee.c
@@ -26,6 +26,8 @@ void read_bfee(unsigned char *inBytes, mxArray *outCell)
 	char tmp;
 	int size[] = {Ntx, Nrx, 30};
 	mxArray *csi = mxCreateNumericArray(3, size, mxDOUBLE_CLASS, mxCOMPLEX);
+	int perm_size[] = {1, 3};
+	mxArray *perm = mxCreateNumericArray(2, perm_size, mxDOUBLE_CLASS, mxREAL);
 	double* ptrR = (double *)mxGetPr(csi);
 	double* ptrI = (double *)mxGetPi(csi);
 
@@ -54,8 +56,6 @@ void read_bfee(unsigned char *inBytes, mxArray *outCell)
 	}
 
 	/* Compute the permutation array */
-	int perm_size[] = {1, 3};
-	mxArray *perm = mxCreateNumericArray(2, perm_size, mxDOUBLE_CLASS, mxREAL);
 	ptrR = (double *)mxGetPr(perm);
 	ptrR[0] = ((antenna_sel) & 0x3) + 1;
 	ptrR[1] = ((antenna_sel >> 2) & 0x3) + 1;
@@ -93,6 +93,15 @@ void read_bfee(unsigned char *inBytes, mxArray *outCell)
 void mexFunction(int nlhs, mxArray *plhs[],
 	         int nrhs, const mxArray *prhs[])
 {
+	const char* fieldnames[] = {"timestamp_low",
+		"bfee_count",
+		"Nrx", "Ntx",
+		"rssi_a", "rssi_b", "rssi_c",
+		"noise",
+		"agc",
+		"perm",
+		"rate",
+		"csi"};
 	unsigned char *inBytes;		/* A beamforming matrix */
 	mxArray *outCell;		/* The cellular output */
 
@@ -112,15 +121,6 @@ void mexFunction(int nlhs, mxArray *plhs[],
 	inBytes = mxGetData(prhs[0]);
 
 	/* create the output matrix */
-	const char* fieldnames[] = {"timestamp_low",
-		"bfee_count",
-		"Nrx", "Ntx",
-		"rssi_a", "rssi_b", "rssi_c",
-		"noise",
-		"agc",
-		"perm",
-		"rate",
-		"csi"};
 	outCell = mxCreateStructMatrix(1, 1, 12, fieldnames);
 
 
